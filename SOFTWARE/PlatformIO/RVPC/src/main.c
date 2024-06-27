395f4ee4fae57433be65678ca6f27425633f87ef
TsvetanUsunov <usunov@olimex.com>
2024-06-27 13:10:23 +0300

    update user manual and demo

---
+++ SOFTWARE/PlatformIO/RVPC/src/main.c
@@ -0,0 +1,42 @@
+#include <ch32v00x.h>
+#include <debug.h>
+
+void NMI_Handler(void)       __attribute__((interrupt("WCH-Interrupt-fast")));
+void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+
+#define BUZZER_DELAY_MS 1
+
+int main(void) {
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	SystemCoreClockUpdate();
+    
+    Delay_Init();
+
+	// GPIO configuration
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+	GPIO_InitTypeDef cfg = {0};
+    cfg.GPIO_Mode = GPIO_Mode_Out_PP;
+   	cfg.GPIO_Pin = GPIO_Pin_4;
+	cfg.GPIO_Speed = GPIO_Speed_10MHz;
+    GPIO_Init(GPIOC, &cfg);
+
+	while (1) {
+		GPIO_WriteBit(GPIOC, GPIO_Pin_4, 1);
+		Delay_Ms(BUZZER_DELAY_MS);
+		GPIO_WriteBit(GPIOC, GPIO_Pin_4, 0);
+		Delay_Ms(BUZZER_DELAY_MS);
+	}
+
+}
+
+void NMI_Handler(void) {
+	
+}
+
+void HardFault_Handler(void) {
+	
+	while (1) {
+		
+	}
+}
---
+++ SOFTWARE/README.md
@@ -0,0 +1,8 @@
+# PlatformIO
+The PlatformIO flder contains demo project which drive the beeper,
+You can open and upload it and you will hear sound.
+
+
+# Tools
+pvpc folder contain all tools you will need to program ESP32-S2-DevKitLiPo-USB to act as programmer and the minichlink which used the ESP32-S2 to program CH32V003
+Follow the instructions in the user manual
